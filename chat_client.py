3c798833eeca5e2e0d60c05104fbd4fe80b54f90
wrh2 <wrh2@pdx.edu>
2015-06-13 21:16:38 -0700

    Added license

--- chat_client.py
+++ chat_client.py
@@ -1,7 +1,22 @@
 """
- An IRC Client programmed in python
+ An IRC Client programmed in python for a project from CS494
  Note: code has been tested with python version 2.7.9
- Programmed by William Harrington for CS494 Programming Project
+
+ Copyright (C) 2015  William Harrington Portland State University
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 """
 
 import socket   # for socket objects
